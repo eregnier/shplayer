b7964e5f59fd257cba5288226d86a03f63efe87b
utopman <utopman@gmail.com>
2016-06-03 14:45:47 +0000

    fix path name for fuzz search

--- manager.py
+++ manager.py
@@ -3,7 +3,7 @@ from tools import Tool
 
 def fuzzygen():
 
-	popen('cd {0};find . -name "*.mp3" > {1}'.format(
+	popen('cd "{0}";find . -name "*.mp3" > {1}'.format(
 		Tool.get('music_folder'),
 		Tool.get('music_database'),
 	))
